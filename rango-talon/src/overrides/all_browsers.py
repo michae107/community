66c2187e817734ef63707462dda5043dfa5231ad
Michael Sasse <michaellavonsasse@gmail.com>
2025-12-12 03:39:10 -0600

    update

---
+++ rango-talon/src/overrides/all_browsers.py
@@ -0,0 +1,30 @@
+from talon import Context, actions
+
+from ..command import run_simple_command
+
+ctx = Context()
+ctx.matches = r"""
+tag: browser
+app: chrome
+app: brave
+app: vivaldi
+app: microsoft_edge
+app: opera
+app: safari
+app: firefox
+"""
+
+
+@ctx.action_class("browser")
+class BrowserActions:
+    def go_back():
+        run_simple_command("historyGoBack")
+
+    def go_forward():
+        run_simple_command("historyGoForward")
+
+
+@ctx.action_class("user")
+class UserActions:
+    def tab_duplicate():
+        run_simple_command("cloneCurrentTab")
